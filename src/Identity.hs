ae4d6a1d0f1ea8794fc521d03c4fbf97ae7ec298
Fabian Heredia Montiel <fabianherediajr@gmail.com>
2018-04-30 21:32:48 -0500

    Mejora de Identidad
    Separa a sus propias funciones prettyPrint y añade un parser.

--- src/Identity.hs
+++ src/Identity.hs
@@ -1,19 +1,34 @@
 module Identity where
 
-import Data.ByteString
-import Data.ByteString.Char8
-import Data.ByteString.Base64
-import Data.ByteArray
+import Data.List              as List
+import Data.ByteString        as BS
+import Data.ByteString.Char8  as BS.C8
+import Data.ByteString.Base64 as B64
+import Data.ByteArray         as BA
+import Crypto.Error
 import Crypto.PubKey.Ed25519
 
-data Identity = Identity (Maybe SecretKey) PublicKey
+data Identity = Identity (Maybe SecretKey) PublicKey deriving (Eq, Show)
 
-instance Show Identity where
-  -- | show Identity. @{base64(PublicKey)}.ed25519
-  -- | Example: @+DOl55sJo1wBqWtRuI3otubyE4M24q34X4wikX+7BXg=.ed25519
-  -- >>> sk <- generateSecretKey
-  -- >>> (Identity (Just sk) (toPublic sk))
-  -- ...
-  show (Identity _ pk) = "@" ++
-                         Data.ByteString.Char8.unpack (encode $ Data.ByteString.pack $ Data.ByteArray.unpack pk) ++
-                         ".ed25519"
+-- | prettyPrint Identity. @{base64(PublicKey)}.ed25519
+-- | Example: @+DOl55sJo1wBqWtRuI3otubyE4M24q34X4wikX+7BXg=.ed25519
+--
+-- >>> sk <- generateSecretKey
+-- >>> prettyPrint (Identity (Just sk) (toPublic sk))
+-- ...
+prettyPrint :: Identity -> String
+prettyPrint (Identity _ pk) = "@" ++ encodedPubKey ++ ".ed25519"
+  where encodedPubKey = BS.C8.unpack (encode $ BS.pack $ BA.unpack pk)
+
+
+parseIdentity :: String -> Maybe Identity
+parseIdentity str = if "@" `List.isPrefixOf` str && ".ed25519" `List.isSuffixOf` str then
+                      let encodedPubKeyStr = List.take (List.length str - 9) . List.drop 1 $ str in
+                      let encodedPubKey = decode $ BS.C8.pack encodedPubKeyStr in
+                      case encodedPubKey of
+                        Right encodedPubKey' -> case publicKey encodedPubKey' of
+                                                  CryptoPassed pk' -> Just $ Identity Nothing pk'
+                                                  CryptoFailed _   -> Nothing
+                        Left _ -> Nothing
+                    else
+                      Nothing
